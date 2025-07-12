837b1bde84c6f8756633e81ca516d5ea64014ef0
NhatTrinhLe <nhattrinh.le.it@gmail.com>
2025-07-12 16:48:31 +0700

    init: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const App: React.FC = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
